7f674fb26aa78a7ab62c8fe3d2cffaf9a7f63f3e
Thomas Chardon <chardon.thomas.pro@gmail.com>
2021-06-18 11:00:53 +0200

    switch branche master

---
+++ backend/data/acces.js
@@ -0,0 +1,8 @@
+const datas = {
+    User : "Dumbledore",
+    Password : "FortunaMajor",
+    Cluster : "clustergofullstack.a6q0g.mongodb.net",
+    Database : "myFirstDatabase",
+};
+
+module.exports = datas;
