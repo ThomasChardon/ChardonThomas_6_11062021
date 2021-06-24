ed4374bc607b22e6c18727fdcefe29a6db518e11
Thomas Chardon <chardon.thomas.pro@gmail.com>
2021-06-24 08:35:28 +0200

    modifications de derniere minute

--- backend/data/acces.js
+++
@@ -1,8 +0,0 @@
-const datas = {
-    User : "Dumbledore",
-    Password : "FortunaMajor",
-    Cluster : "clustergofullstack.a6q0g.mongodb.net",
-    Database : "myFirstDatabase",
-};
-
-module.exports = datas;
